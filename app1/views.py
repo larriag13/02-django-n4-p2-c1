d03e04f5579b801a7d72feca6f2fa9db5d726925
larriag13 <luis.arriagada13@inacapmail.cl>
2026-08-19 15:55:53 -0400

    vista 1 app1 terminada

---
+++ app1/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def vista1(request):
+    return HttpResponse("<h1>Vista 1 App1</h1>"
+    "<p style='color:blue'>Todo lo que necesitas</p>")
